46f538340678e0bc26526b727d9ad7ef6ffebe88
k1k3cb <enriquecbar@gmail.com>
2024-01-02 22:34:01 +0100

    summary

--- src/components/cardRight/CardRight.jsx
+++ src/components/cardRight/CardRight.jsx
@@ -1,3 +1,4 @@
+import Button from '../button/Button';
 import RectangleInfo from '../rectangleInfo/RectangleInfo';
 import styles from './CardRight.module.css';
 
@@ -6,8 +7,35 @@ const CardRight = () => {
 		<div className={styles.container}>
 			<div className={styles.header}>Summary</div>
 			<div>
-				<RectangleInfo />
+				<RectangleInfo
+					title='Reaction'
+					icon='/public/assets/images/icon-reaction.svg'
+					points={80}
+					color='red'
+				/>
+
+				<RectangleInfo
+					title='Memory'
+					icon='/public/assets/images/icon-memory.svg'
+					points={92}
+					color='yellow'
+				/>
+
+				<RectangleInfo
+					title='Verbal'
+					icon='/public/assets/images/icon-verbal.svg'
+					points={61}
+					color='green'
+				/>
+
+				<RectangleInfo
+					title='Visual'
+					icon='/public/assets/images/icon-visual.svg'
+					points={73}
+					color='blue'
+				/>
 			</div>
+			<Button />
 		</div>
 	);
 };
--- src/components/rectangleInfo/RectangleInfo.jsx
+++ src/components/rectangleInfo/RectangleInfo.jsx
@@ -1,14 +1,17 @@
-import styles from './RectangleInfo.module.css'
+import styles from './RectangleInfo.module.css';
 
-const RectangleInfo = () => {
-  return (
-    <div className={styles.container}>
+const RectangleInfo = ({ title, icon, color, points }) => {
+	return (
+		<div className={`${styles.container}  ${styles[color]}`}>
+			<p className={`${styles[color]}-text`}>
+				<img src={icon} alt={title} />
+				{title}
+			</p>
+			<div className={styles.result}>
+				<span className={styles.points}>{points} </span>/100
+			</div>
+		</div>
+	);
+};
 
-        <p><span>Icono </span>Reaction</p>
-        <div><span>80 </span>/100</div>
-
-    </div>
-  )
-}
-
-export default RectangleInfo
+export default RectangleInfo;
