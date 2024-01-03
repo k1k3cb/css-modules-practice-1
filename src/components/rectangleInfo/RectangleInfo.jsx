31f20299874ac590bc364c29ce12489ec85a1dac
k1k3cb <enriquecbar@gmail.com>
2024-01-03 12:45:01 +0100

    card styles mod

--- src/components/rectangleInfo/RectangleInfo.jsx
+++ src/components/rectangleInfo/RectangleInfo.jsx
@@ -3,7 +3,7 @@ import styles from './RectangleInfo.module.css';
 const RectangleInfo = ({ title, icon, color, points }) => {
 	return (
 		<div className={`${styles.container}  ${styles[color]}`}>
-			<p className={`${styles[color]}-text`}>
+			<p className={`${styles[color + '-text']}`}>
 				<img src={icon} alt={title} />
 				{title}
 			</p>
